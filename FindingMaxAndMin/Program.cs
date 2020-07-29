bd815087abcc1af2f1873056e78a45a219a6d9e5
Barbara Gska <pacuszka.barbara@gmail.com>
2020-07-29 10:33:31 +0200

    Adding project files

---
+++ FindingMaxAndMin/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingMaxAndMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Podaj ciąg liczb rozdzielając je spacją");
+
+            var numbers = CreatingList.CreateList(Console.ReadLine());
+
+            var min = numbers.Min();
+            var max = numbers.Max();
+
+            Console.WriteLine($"{max.ToString()} {min.ToString()}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
